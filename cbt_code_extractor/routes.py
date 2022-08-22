184eaf3b819f1d7fbec6a69a35b01210c25b3b39
zubairwazir <zubairwazir777@gmail.com>
2022-08-22 11:22:01 +0500

    download and thankyou routes added

--- cbt_code_extractor/routes.py
+++ cbt_code_extractor/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, flash, jsonify
 from cbt_code_extractor import app, cnx
+import pandas as pd
 
 
 @app.route('/')
@@ -11,16 +12,26 @@ def home():
 def index():
     if request.method == 'POST':
         codes = request.form['cbt_code']
-        cursor = cnx.cursor()
-        cursor.execute("SELECT * FROM cbt_code where id IN (%s)" % codes)
-        result = cursor.fetchall()
-    return render_template('download.html', result=result)
+        if codes:
+            cursor = cnx.cursor()
+            cursor.execute("SELECT * FROM cbt_code where id IN (%s)" % codes)
+            result = cursor.fetchall()
+            if result:
+                headers = ['Code', 'IntlOpenId']
+                df = pd.DataFrame(data=result, columns=headers)
+                df.to_csv(r'/home/zubair/Desktop/dashboard/Upwork Ongoing/CBT-Code-Extractor/exported_data.csv',
+                          index=False)
+            else:
+                flash("CBT codes not found in Database!", category='danger')
+                return render_template('index.html')
+        else:
+            error = 'Please Enter Valid CBT Codes!'
+            return render_template('index.html', error=error)
+        return render_template('download.html', result=result)
 
 
-@app.route('/download', methods=['GET', 'POST'])
+@app.route('/download')
 def download():
-    cnx.cursor().close()
-    cnx.close()
-    return render_template('download.html')
+    return render_template('thankyou.html')
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', debug=True)
